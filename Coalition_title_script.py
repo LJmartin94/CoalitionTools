e71426ea55b2156facee560eb2a0edfb3dc09839
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-11-30 20:56:28 +0100

    Leaderboard prints student id, rank, the title_id equipped, the login name, the score, and where the student is in the building if applicable.

--- Coalition_title_script.py
+++ Coalition_title_script.py
@@ -28,6 +28,8 @@ def give_coalition_titles(coalition_id):
     # per student it has fields for a student's id, rank, 'abstract_title', intra title_id, and username.
     # The actual title_id and username are added later, these initially just have placeholders.
     student_rank_info = make_student_rank_info(snapshot_bundle)
+    coalition_titles = make_title_id_array(coalition_id)
+    student_rank_info = append_equipped_titles(student_rank_info, coalition_titles)
 
     # To print an optional summary of who has what title, set print_summary to 1 in the above global variables.
     # This does slow down the program however,
@@ -36,6 +38,24 @@ def give_coalition_titles(coalition_id):
         student_rank_info = append_login_names(student_rank_info)
         for entry in student_rank_info:
             print(entry)
+
+
+def make_title_id_array(coalition_id):
+    title_id_array = [0 for _ in range(36)]
+    coalition_spec = 'vela'
+    if coalition_id == cetus_coalition_id:
+        coalition_spec = 'cetus'
+    if coalition_id == pyxis_coalition_id:
+        coalition_spec = 'pyxis'
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    with open(base_dir + '/title_config.yml', 'r') as cfg_stream:
+        config = yaml.load(cfg_stream, Loader=yaml.BaseLoader)
+        x = 0
+        for _ in range(36):
+            title_id_array[x] = config['all_titles'][coalition_spec][str(x + 1)]
+            x += 1
+    return title_id_array
+
 
 
 def make_coalition_state_snapshot(coalition_id) -> object:
@@ -65,7 +85,7 @@ def make_student_rank_info(snapshot_bundle):
     student_rank_info = [[] for _ in range(number_of_students)]
     x = 0
     for entry in coalition_snapshot:
-        student_rank_info[x] = [entry['user_id'], entry['rank'], "title_id", "username", entry['score'], "location"]
+        student_rank_info[x] = [entry['user_id'], entry['rank'], "titles: ", "username", entry['score'], "location"]
         x += 1
     # Sort the list by rank because why not
     student_rank_info = sort_by_rank(student_rank_info)
@@ -89,8 +109,36 @@ def append_login_names(student_rank_info):
             if kvp['id'] == entry[0]:
                 entry[3] = kvp['login']
                 entry[5] = kvp['location']
+                # titles = get_student_title(kvp['id'])
+                # for title in titles:
+                #     entry[2] = entry[2] + " " + title['name'] + ","
     return student_rank_info
 
+
+def append_equipped_titles(student_rank_info, titles):
+    for title_id in titles:
+        title_owners = get_ids_of_students_with_title(title_id)
+        for user in title_owners:
+            if user['selected'] == True:
+                for student in student_rank_info:
+                    if student[0] == user['user_id']:
+                        student[2] = student[2] + str(user['title_id'])
+    return student_rank_info
+
+def get_ids_of_students_with_title(title_id):
+    ic.progress_bar = False
+    payload = {
+    }
+    data = ic.pages_threaded("titles/" + str(title_id) + "/titles_users", params=payload)
+    return data
+
+
+def get_student_title(student_id):
+    ic.progress_bar = False
+    payload = {
+    }
+    data = ic.pages_threaded("users/" + str(student_id) + "/titles", params=payload)
+    return data
 
 if __name__ == "__main__":
     main()
